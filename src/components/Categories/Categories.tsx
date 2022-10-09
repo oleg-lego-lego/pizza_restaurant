b45d19aea8dcd37116be3eefdbd364481620319a
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-10-09 18:49:07 +0300

    refactoring logic onClickCategory in Categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 export const Categories = () => {
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
     const [activeIndex, setActiveIndex] = useState(0)
 
     const onClickCategory = (activeButton: number) => {
@@ -10,12 +11,9 @@ export const Categories = () => {
     return (
         <div className="categories">
             <ul>
-                <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? "active" : ''}>Все</li>
-                <li onClick={() => onClickCategory(1)} className={activeIndex === 1 ? "active" : ''}>Мясные</li>
-                <li onClick={() => onClickCategory(2)} className={activeIndex === 2 ? "active" : ''}>Вегетарианская</li>
-                <li onClick={() => onClickCategory(3)} className={activeIndex === 3 ? "active" : ''}>Гриль</li>
-                <li onClick={() => onClickCategory(4)} className={activeIndex === 4 ? "active" : ''}>Острые</li>
-                <li onClick={() => onClickCategory(5)} className={activeIndex === 5 ? "active" : ''}>Закрытые</li>
+                {categories.map((el, i) => (
+                    <li onClick={() => onClickCategory(i)} className={activeIndex === i ? "active" : ''}>{el}</li>
+                ))}
             </ul>
         </div>
     )
