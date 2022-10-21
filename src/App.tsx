872a0a5ee21fd12e90ca4d67d538f27f163d1af2
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-10-21 10:39:51 +0300

    created counterSlice

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,42 @@ import {NotFound} from "./pages/NotFound";
 import {Route, Routes} from "react-router-dom";
 import {Cart} from "./pages/Cart";
 import {AppContext} from "./components/Context/AppContext";
+import { useSelector, useDispatch } from 'react-redux'
+import {RootState} from "./redux/store";
+import { decrement, increment } from './redux/slices/filterSlice'
+
 
 
 
 function App() {
     const [searchValue, setSearchValue] = useState('')
 
+    const count = useSelector((state: RootState) => state.counter.value)
+    const dispatch = useDispatch()
+
     return (
         <div className="wrapper">
+
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
+
+
+
+
+
             <AppContext.Provider value={{searchValue, setSearchValue}}>
                 <Header/>
                 <div className="content">
