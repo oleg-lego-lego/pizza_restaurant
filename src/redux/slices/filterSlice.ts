965b2d32fd0ed2f323e6b0f4203aae0e9d412e8a
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-10-21 17:14:13 +0300

    created filterSlice

--- src/redux/slices/filterSlice.ts
+++ src/redux/slices/filterSlice.ts
@@ -1,30 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit'
+import type {PayloadAction} from '@reduxjs/toolkit'
 
-export interface CounterState {
-    value: number
+export interface CounterStateType {
+    categoryId: number,
+    sort: { name: string, sortProperty: string }
 }
 
-const initialState: CounterState = {
-    value: 0,
+
+const initialState: CounterStateType = {
+    categoryId: 0,
+    sort: {name: 'популярности', sortProperty: 'rating'}
 }
 
-export const counterSlice = createSlice({
-    name: 'counter',
+const filterSlice = createSlice({
+    name: 'filters',
     initialState,
     reducers: {
-        increment: (state) => {
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        },
-        incrementByAmount: (state, action: PayloadAction<number>) => {
-            state.value += action.payload
-        },
-    },
+        setCategoryId(state: any, action: any) {
+            state.categoryId = action.payload
+        }
+    }
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const {setCategoryId} = filterSlice.actions
 
-export default counterSlice.reducer
+export default filterSlice.reducer;
+
+//
+// export const counterSlice = createSlice({
+//     name: 'counter',
+//     initialState,
+//     reducers: {
+//         increment: (state) => {
+//             state.value += 1
+//         },
+//         decrement: (state) => {
+//             state.value -= 1
+//         },
+//         incrementByAmount: (state, action: PayloadAction<number>) => {
+//             state.value += action.payload
+//         },
+//     },
+// })
+//
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions
+//
+// export default counterSlice.reducer
+
