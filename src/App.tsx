79508edaeee1aa33566ce37436c82a0b001491ed
oleg ostrovski <oleg.ostrovski@gmail.com>
2023-02-23 14:26:44 +0300

    add FullPizza component + Route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Home} from "./pages/Home";
 import {NotFound} from "./pages/NotFound";
 import {Route, Routes} from "react-router-dom";
 import {Cart} from "./pages/Cart";
+import {FullPizza} from "./pages/FullPizza/FullPizza";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
                 <Routes>
                     <Route path={'/'} element={<Home/>}/>
                     <Route path={'/cart'} element={<Cart/>}/>
+                    <Route path={'/pizza'} element={<FullPizza/>}/>
                     <Route path={'*'} element={<NotFound/>}/>
                 </Routes>
             </div>
---
+++ src/pages/FullPizza/FullPizza.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const FullPizza = () => {
+    return (
+        <div>
+            <h2>qwrwer</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid animi aperiam asperiores
+                blanditiis cum deserunt distinctio dolore error excepturi exercitationem fugit id impedit iste iusto
+                libero maxime numquam quis quisquam quod, recusandae rem sequi sunt velit veniam veritatis, vero, vitae!
+                Atque commodi earum fugiat, illum laudantium magni, odit praesentium quaerat quos repudiandae saepe
+                sint, tempora veniam. Animi atque consectetur consequuntur cum dicta dignissimos dolor dolorem dolores
+                dolorum ea eum, eveniet fugit hic illo in inventore ipsam iure labore molestiae nam neque nostrum nulla
+                omnis perferendis praesentium provident, qui quos rem repellat similique tempore totam ut voluptates. A
+                accusamus accusantium, adipisci consequuntur eius et excepturi hic id modi nobis nostrum obcaecati optio
+                perferendis possimus quam quis quo reprehenderit, sequi similique soluta sunt tempora tempore. Debitis
+                deleniti ipsum porro suscipit. Ad ducimus eius laboriosam minima officia ratione saepe tempore vel!
+                Adipisci beatae maiores quos sapiente soluta.</p>
+            <h4>asfsad</h4>
+        </div>
+    );
+};
